a8ec7e076017b9cd36473122cd963509c6857156
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-07 07:45:11 +0000

    Update docker_install_desktop.sh

--- docker_install_desktop.sh
+++ docker_install_desktop.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-r=curl https://docs.docker.com/desktop/install/ubuntu/ |grep DEB |head -1| grep -o -P '(?<=href=").*(?=" class="button primary-btn)'
-curl -kLs --compressed "$r"
+f=$(curl https://docs.docker.com/desktop/install/ubuntu/ |grep DEB |head -1| grep -o -P '(?<=href=").*(?=\?utm_source)')
+
+echo "dl'ing: $f"
+
+wget $f
