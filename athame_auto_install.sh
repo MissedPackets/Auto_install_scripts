f0620ab71de9c47f7029b47336cc03d22ede3a79
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-11 21:58:41 +0000

    Update athame_auto_install.sh

--- athame_auto_install.sh
+++ athame_auto_install.sh
@@ -3,4 +3,4 @@ git clone --recursive http://github.com/ardagnir/athame
 cd athame
 ./readline_athame_setup.sh --libdir=/lib/x86_64-linux-gnu --use_sudo --vimbin=$(which nvim)
 
-./bash_readline_setup.sh --use_sudo --vimbin=$(which nvim)
+./bash_readline_setup.sh --use_sudo
