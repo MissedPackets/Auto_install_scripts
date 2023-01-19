c79407721b2fc25d786a43ce2e7ec86785a141cd
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-19 11:26:23 +0000

    Create nix_pkg_manager_installer.sh

---
+++ nix_pkg_manager_installer.sh
@@ -0,0 +1,9 @@
+#!/usr/bin
+sudo apt install nix-bin
+a=$(read -p)
+echo "Installing Nix's package manager..."
+sh <(curl -L https://nixos.org/nix/install) --no-daemon
+#Incase there is a sqlite error .e.g) SQLite database '/nix/var/nix/db/db.sqlite' is busy...
+sandbox = false > ~/.config/nix/nix.conf
+
+echo -e "Nix's package manager has finished installing! Try to run this command to test if there are no errors: \n nix-env -i hello"
