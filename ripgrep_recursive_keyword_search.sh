0c26e0ca7028fa59b0c290f03aca0d8af8a46229
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-19 14:16:10 +0000

    Update ripgrep_recursive_keyword_search.sh

--- ripgrep_recursive_keyword_search.sh
+++ ripgrep_recursive_keyword_search.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Error: Please provide a keyword as an argument"
+  exit 1
+fi
+
+keyword=$1
+
 # does the same as grep -rn ./ -e "keyword"
-rg -n --no-ignore -e 'keyword' ./
+rg -n --no-ignore -e "$keyword" ./
