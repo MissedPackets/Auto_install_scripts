6266b4c6f590e51a3e400d0e15cc7dcf32b73ab1
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-20 13:36:49 +0000

    Update nix_pkg_manager_installer.sh

--- nix_pkg_manager_installer.sh
+++ nix_pkg_manager_installer.sh
@@ -1,4 +1,9 @@
 #!/usr/bin
+sudo install -d -m755 -o $(id -u) -g $(id -g) /nix
+
+# If this all doesn't work, uncomment this to install the stable ver. of nix's pkg manager
+#curl -L https://nixos.org/nix/install | sh
+
 unset NIX_REMOTE
 sudo apt install nix-bin
 a=$(read -p)
@@ -9,3 +14,4 @@ sandbox = false > ~/.config/nix/nix.conf
 
 echo -e "Nix's package manager has finished installing! Try to run this command to test if there are no errors: \n nix-env -i hello"
 unset NIX_REMOTE
+source $HOME/.nix-profile/etc/profile.d/nix.sh
