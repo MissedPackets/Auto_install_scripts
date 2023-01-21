fb159ebeebfc0883dc1333947a0f2806527ac1aa
lostpackets <119964688+lostpackets@users.noreply.github.com>
2023-01-21 11:25:06 +0000

    Update nix_pkg_manager_installer.sh

--- nix_pkg_manager_installer.sh
+++ nix_pkg_manager_installer.sh
@@ -12,6 +12,10 @@ sh <(curl -L "https://releases.nixos.org/nix/nix-2.9.1/install") --no-daemon
 #Incase there is a sqlite error .e.g) SQLite database '/nix/var/nix/db/db.sqlite' is busy...
 sandbox = false > ~/.config/nix/nix.conf
 
-echo -e "Nix's package manager has finished installing! Try to run this command to test if there are no errors: \n nix-env -i hello"
+# This needs to run so that the single user has most access to the contents of /nix dir
+sudo chown -R $USER:staff /nix
+
+echo -e "Nix's package manager has finished installing!\nTry to run this command to test if there are no errors: \n nix-env -i hello\nOtherwise, if it is unstable and the version is x>2.9.1, re-run this script so that it updates the version."
 unset NIX_REMOTE
 source $HOME/.nix-profile/etc/profile.d/nix.sh
+
